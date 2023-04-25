7dc807caa87325400e8a3a064845f7e37cfbaeef
Clara Bernadou <clarabernadou56690@gmail.com>
2023-04-25 21:33:17 +0200

    Get and display user data in Header

---
+++ frontend/src/componants/dashboardSection/header/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Header(userData) {
+    console.log(userData);
+  return (
+    <div className="headerContainer">
+    </div>
+  );
+}
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,27 @@
-import NavTop from "../componants/navigationSection/navTop/NavTop";
+import React, { useEffect, useState } from "react";
+
 import NavLeft from "../componants/navigationSection/navLeft/NavLeft";
+import NavTop from "../componants/navigationSection/navTop/NavTop"
+import Header from "../componants/dashboardSection/header/Header";
 
 export default function Home() {
+    const [userData, setUserData] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetch('http://localhost:3000/user/18');
+            const data = await response.json();
+            setUserData(data);
+        };
+        fetchData();
+    }, []);
+
     return (
         <>
             <NavTop />
+            <Header userData={userData} />
             <NavLeft />
         </>
-      );
+    );
 }
+
