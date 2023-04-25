34832969a5597dd5332f717daf629843c2342ce9
Clara Bernadou <clarabernadou56690@gmail.com>
2023-04-25 21:51:31 +0200

    Get user data and display it + add comments

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,23 +5,24 @@ import NavTop from "../componants/navigationSection/navTop/NavTop"
 import Header from "../componants/dashboardSection/header/Header";
 
 export default function Home() {
-    const [userData, setUserData] = useState(null);
+  const [userData, setUserData] = useState(null); // Define a state variable named 'userData' and initialize it to 'null'
 
-    useEffect(() => {
-        const fetchData = async () => {
-            const response = await fetch('http://localhost:3000/user/18');
-            const data = await response.json();
-            setUserData(data);
-        };
-        fetchData();
-    }, []);
+  useEffect(() => {
+    // Declare an async function named 'fetchData' to fetch user data from the server
+    const fetchData = async () => {
+      const response = await fetch('http://localhost:3000/user/18'); // Send a GET request to the server for user data with an ID
+      const data = await response.json(); // Convert the response to JSON format
+      setUserData(data.data); // Set the state variable 'userData' to the fetched data
+    };
+    fetchData(); // Call the 'fetchData' function when the component mounts
+  }, []);
 
-    return (
-        <>
-            <NavTop />
-            <Header userData={userData} />
-            <NavLeft />
-        </>
-    );
+  return (
+    <>
+      <NavTop />
+      {/* Pass the user data to the 'Header' component as a prop */}
+      <Header userData={userData?.userInfos} /> {/* Use the safety operator to avoid errors */}
+      <NavLeft />
+    </>
+  );
 }
-
