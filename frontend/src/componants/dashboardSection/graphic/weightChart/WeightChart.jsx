8ea1e22fffbe34117386294644f652f84f5e25cd
Clara Bernadou <clarabernadou56690@gmail.com>
2023-04-26 22:08:51 +0200

    Completed Daily Activity Graph
    Bar graph that contains two bars, one for weight in kilograms and the other for calories burned. Also uses a custom tooltip to display the data for each bar when the user hovers over it.

--- frontend/src/componants/dashboardSection/graphic/weightChart/WeightChart.jsx
+++ frontend/src/componants/dashboardSection/graphic/weightChart/WeightChart.jsx
@@ -2,6 +2,24 @@ import React from 'react';
 import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 import "./weightChart.css"
 
+const CustomTooltip = ({ active, payload, label }) => {
+  if (active && payload && payload.length) {
+    return (
+      <div className="custom-tooltip">
+        <div style={{ width: 'fit-content', height: 'fit-content', backgroundColor: '#E60000', padding: '5px' }}>
+          <p style={{ color: '#FFFFFF', fontSize: '14px', textAlign: 'center', margin: '15px 5px' }}>
+            {`${payload[0].value} ${payload[0].dataKey === 'kilogram' ? 'kg' : 'Kcal'}`}
+          </p>
+          <p style={{ color: '#FFFFFF', fontSize: '14px', textAlign: 'center', margin: '15px 5px'}}>
+            {`${payload[1].value} ${payload[1].dataKey === 'calories' ? 'Kcal' : 'kg'}`}
+          </p>
+        </div>
+      </div>
+    );
+  }
+  return null;
+};
+
 export default function WeightChart({ userActivity }) {
   let kilogramArray = [];
   let caloriesArray = [];
@@ -24,6 +42,16 @@ export default function WeightChart({ userActivity }) {
           data={userActivity}
           margin={0}
         >
+          <text 
+            x="5" 
+            y="15" 
+            textAnchor="start"
+            fontSize={15} 
+            fontWeight={500}
+            color='#20253A'
+          >
+            Activité quotidienne
+          </text>
           <CartesianGrid vertical={0} strokeDasharray="3" stroke="#DEDEDE" />
           <XAxis 
             dataKey="day" 
@@ -41,13 +69,15 @@ export default function WeightChart({ userActivity }) {
           />
           <YAxis 
             domain={[minCalories - 50, maxCalories + 10]} yAxisId="right" hide={true} />
-          <Tooltip />
+          <Tooltip content={<CustomTooltip />} />
           <Legend 
             align="right" 
             verticalAlign="top" 
-            wrapperStyle={{ paddingBottom: 50 }}  
-            iconType="circle" iconSize={7} 
-            label={{ fill: '#74798C', fontSize: 14, fontWeight: '500', labelGap: 10 }}
+            wrapperStyle={{ paddingBottom: 50, marginBottom: 30 }}  // Ajout de marginBottom
+            iconType="circle" 
+            iconSize={7} 
+            formatter={(value, entry, index) => <span style={{color: "#74798C", marginLeft: 10, marginRight: 30}}>{value}</span>}
+            labelStyle={{ fontSize: 14, fontWeight: '500' }}
           />
           <Bar dataKey="kilogram" name='Poids (kg)' fill="#282D30" barSize={7} radius={[4, 4, 0, 0]}  />
           <Bar dataKey="calories" name='Calories brûlées (kCal)' fill="#E60000" barSize={7} radius={[4, 4, 0, 0]} yAxisId="right" />
