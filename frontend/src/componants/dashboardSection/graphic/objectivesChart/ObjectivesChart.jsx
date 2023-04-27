a5347a847f0f86d9e23eba0399b979d628887a75
Clara Bernadou <clarabernadou56690@gmail.com>
2023-04-27 22:53:19 +0200

    Remove vertical line from dot chart

--- frontend/src/componants/dashboardSection/graphic/objectivesChart/ObjectivesChart.jsx
+++ frontend/src/componants/dashboardSection/graphic/objectivesChart/ObjectivesChart.jsx
@@ -1,20 +1,10 @@
-import React, { useState } from 'react';
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import React from 'react';
+import { LineChart, Line, XAxis, YAxis, Tooltip, ResponsiveContainer } from 'recharts';
 import "./objectivesChart.css"
 
 const weekDays = ['L', 'M', 'M', 'J', 'V', 'S', 'D'];
 
-const CustomDot = ({ cx, cy, value, onMouseOver, onMouseOut }) => {
-    const [show, setShow] = useState(true);
-    return (
-      <g>
-        {show && <circle cx={cx} cy={cy} r={5} fill="#FFFFFF" onMouseOver={onMouseOver} onMouseOut={onMouseOut} />}
-        {show && setShow(false)}
-      </g>
-    );
-};
-
-const CustomTooltip = ({ active, payload, label }) => {
+const CustomTooltip = ({ active, payload }) => {
     if (active && payload && payload.length) {
       return (
         <div className="custom-tooltip" style={{backgroundColor: "#FFFFFF" }}>
@@ -26,7 +16,6 @@ const CustomTooltip = ({ active, payload, label }) => {
     }
     return null;
 };
-  
 
 export default function ObjectivesChart({ userAverageSessions }) {
     let sessionsArray = [];
@@ -44,22 +33,22 @@ export default function ObjectivesChart({ userAverageSessions }) {
         <div className='objectivesChartContainer'>
             <ResponsiveContainer width="100%" height="100%">
                 <LineChart width={300} height={100} data={userAverageSessions}>
-                <text 
-                    x="5" 
-                    y="15" 
-                    textAnchor="start"
-                    fontSize={15} 
-                    fontWeight={500}
-                    color='#FFFFFF'
-                    opacity={0.5}
-                >
-                    <tspan x="5" dy="0">{`Durée moyenne des`}</tspan>
-                    <tspan x="5" dy="20">{`sessions`}</tspan>
-                </text>
-                    <Line type="monotone" dataKey="sessionLength" stroke="#FFFFFF" strokeWidth={2} yAxisId="right" dot={<CustomDot />} />
-                    <XAxis tickFormatter={(day) => weekDays[day]} axisLine={false} tickLine={false} tick={{ fill: '#FFFFFF', opacity: '0.5' }} />
-                    <YAxis domain={[minSession - 5, maxSession + 5]} yAxisId="right" hide={true} />
-                    <Tooltip content={<CustomTooltip />} />
+                  <text 
+                      x="5" 
+                      y="15" 
+                      textAnchor="start"
+                      fontSize={15} 
+                      fontWeight={500}
+                      fill='#FFFFFF'
+                      opacity={0.5}
+                  >
+                      <tspan x="5" dy="0">{`Durée moyenne des`}</tspan>
+                      <tspan x="5" dy="20">{`sessions`}</tspan>
+                  </text>
+                  <Line type="monotone" dataKey="sessionLength" stroke="#FFFFFF" strokeWidth={2} yAxisId="right" dot={false} />
+                  <XAxis tickFormatter={(day) => weekDays[day]} axisLine={false} tickLine={false} tick={{ fill: '#FFFFFF', opacity: '0.5' }} />
+                  <YAxis domain={[minSession - 5, maxSession + 5]} yAxisId="right" hide={true} />
+                  <Tooltip content={<CustomTooltip />} />
                 </LineChart>
             </ResponsiveContainer>          
         </div>
