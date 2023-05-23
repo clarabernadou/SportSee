f5600de88278f1d70c608c5a3a6f12583f9ab0e8
Clara Bernadou <clarabernadou56690@gmail.com>
2023-05-24 00:32:58 +0200

    Adding modeling classes

--- frontend/src/DataProvider.jsx
+++ frontend/src/DataProvider.jsx
@@ -5,6 +5,38 @@ const DataContext = createContext(); // Creating a new context for data
 const userId = process.env.REACT_APP_USER_ID; // Getting the user ID from environment variables
 const useMockData = process.env.REACT_APP_MOCK_DATA === "true"; // Checking if mock data should be used
 
+class UserMainDataModel {
+  constructor(id, userInfos, score, todayScore, keyData) {
+    this.id = id;
+    this.userInfos = userInfos;
+    this.score = score || todayScore;
+    this.todayScore = todayScore;
+    this.keyData = keyData;
+  }
+}
+
+class UserActivityModel {
+  constructor(userId, sessions) {
+    this.userId = userId;
+    this.sessions = sessions;
+  }
+}
+
+class UserAverageSessionsModel {
+  constructor(userId, sessions) {
+    this.userId = userId;
+    this.sessions = sessions;
+  }
+}
+
+class UserPerformanceModel {
+  constructor(userId, kind, data) {
+    this.userId = userId;
+    this.kind = kind;
+    this.data = data;
+  }
+}
+
 export const DataProvider = ({ children }) => {
   // State variables to store the fetched data
   const [userData, setUserData] = useState(null);
@@ -18,11 +50,34 @@ export const DataProvider = ({ children }) => {
         console.log("The code uses mocked data");
         try {
           const data = await import('./data/data.json'); // Importing mocked data from the local file
-          // Setting the state variables with the fetched data based on the user ID
-          setUserData(data.userMainData.find((user) => user.id === parseInt(userId)));
-          setUserActivity(data.userActivity.find((user) => user.userId === parseInt(userId)));
-          setUserAverageSessions(data.userAverageSession.find((user) => user.userId === parseInt(userId)));
-          setUserPerformance(data.userPerformance.find((user) => user.userId === parseInt(userId)));
+
+          const userMainData = data.userMainData.map(user => new UserMainDataModel(
+            user.id,
+            user.userInfos,
+            user.score,
+            user.todayScore,
+            user.keyData
+          ));          
+          const userActivityData = data.userActivity.map(activity => new UserActivityModel(
+            activity.userId,
+            activity.sessions
+          ));
+          const userAverageSessionData = data.userAverageSession.map(session => new UserAverageSessionsModel(
+            session.userId,
+            session.sessions
+          ));
+          const userPerformanceData = data.userPerformance.map(performance => new UserPerformanceModel(
+            performance.userId,
+            performance.kind,
+            performance.data
+          ));
+
+          console.log(userMainData);
+
+          setUserData(userMainData.find((user) => user.id === parseInt(userId)));
+          setUserActivity(userActivityData.find((user) => user.userId === parseInt(userId)));
+          setUserAverageSessions(userAverageSessionData.find((user) => user.userId === parseInt(userId)));
+          setUserPerformance(userPerformanceData.find((user) => user.userId === parseInt(userId)));
         } catch (error) {
           console.error(error);
         }
@@ -32,19 +87,25 @@ export const DataProvider = ({ children }) => {
           // Fetching the data from the server using the user ID
           const responseUserData = await fetch(`http://localhost:3000/user/${userId}`);
           const userData = await responseUserData.json();
-          setUserData(userData.data);
+          setUserData(new UserMainDataModel(
+            userData.data.id,
+            userData.data.userInfos,
+            userData.data.score,
+            userData.data.todayScore,
+            userData.data.keyData
+          ));
 
           const responseUserActivity = await fetch(`http://localhost:3000/user/${userId}/activity`);
           const userActivity = await responseUserActivity.json();
-          setUserActivity(userActivity.data);
+          setUserActivity(new UserActivityModel(userActivity.data.userId, userActivity.data.sessions));
 
           const responseAverageSessions = await fetch(`http://localhost:3000/user/${userId}/average-sessions`);
           const averageSessions = await responseAverageSessions.json();
-          setUserAverageSessions(averageSessions.data);
+          setUserAverageSessions(new UserAverageSessionsModel(averageSessions.data.userId, averageSessions.data.sessions));
 
           const responsePerformance = await fetch(`http://localhost:3000/user/${userId}/performance`);
           const performanceData = await responsePerformance.json();
-          setUserPerformance(performanceData.data);
+          setUserPerformance(new UserPerformanceModel(performanceData.data.userId, performanceData.data.kind, performanceData.data.data));
         } catch (error) {
           console.error(error);
         }
@@ -54,16 +115,11 @@ export const DataProvider = ({ children }) => {
     fetchData(); // Call the data fetching function
   }, []);
 
-  const renamedUserData = userData && {
-    ...userData,
-    score: userData.todayScore
-  };
-
   // Providing the data through the context provider
   return (
     <DataContext.Provider
       value={{
-        userData: renamedUserData,
+        userData,
         userActivity,
         userAverageSessions,
         userPerformance,
